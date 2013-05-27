7b3c4ceee1c87c6583941831801845a4e815c244
Miles Wu <miles.wu@wu-home.co.uk>
2013-05-27 03:00:03 +0000

    Deal with proper chunking; verified no memory leaks

--- main.c
+++ main.c
@@ -10,61 +10,93 @@
 #include "http-parser/http_parser.h"
 
 #define PORT 8081
+#define READSIZE 1024
+#define DYNAMICSTRINGSIZE 1024
 
-struct http_parser_data {
+typedef struct {
+	char *str;
+	size_t size;
+	size_t alloc_size;
+} dynamic_string;
+
+struct client_socket_data {
 	struct ev_loop *loop;
 	ev_io *watcher;
+	int sock;
+	http_parser *parser;
+	http_parser_settings parser_settings;
+	dynamic_string *url;
 };
 
+dynamic_string *dynamic_string_init() {
+	dynamic_string *str = (dynamic_string *)malloc(sizeof(dynamic_string));
+	str->alloc_size = DYNAMICSTRINGSIZE;
+	str->size = 0;
+	str->str = (char*)malloc(sizeof(char)*str->alloc_size);
+	return str;
+}
+
+void dynamic_string_free(dynamic_string *str) {
+	free(str->str);
+	free(str);
+}
+
+void dynamic_string_append(dynamic_string *str, char *append, size_t size) {
+	while(str->size + size > str->alloc_size) {
+		str->str = (char *)realloc((void *)str->str, str->alloc_size*2);
+		str->alloc_size *= 2;
+	}
+
+	memcpy(((void*)str->str) + str->size, (void *)append, size);
+	str->size += size;
+}
+
+static int parser_message_complete_callback(http_parser *parser) {
+	struct client_socket_data *data = (struct client_socket_data*)parser->data;
+
+	printf("URL requested was: %.*s\n", data->url->size, data->url->str);
+
+	const char *reply = "HTTP/1.0 200 OK\r\nContent-Type: text/text\r\nConnection: close\r\nContent-Length: 2\r\n\r\nHi";
+	send(data->sock, reply, strlen(reply), 0);
+
+
+	return 0;
+}
+
 static int parser_url_callback(http_parser *parser, const char *at, size_t length) {
-	printf("URL requested was: %.*s\n", length, at);
-
-	struct http_parser_data *http_parser_data = (struct http_parser_data*)parser->data;
-
-	const char *reply = "HTTP/1.0 200 OK\r\nContent-Type: text/text\r\n\r\nHi";
-	send(http_parser_data->watcher->fd, reply, strlen(reply), 0);
-	ev_io_stop(http_parser_data->loop, http_parser_data->watcher);
-	free(http_parser_data->watcher);
-	close(http_parser_data->watcher->fd);
-	printf("Connection closed\n");
-	
+	struct client_socket_data *data = (struct client_socket_data*)parser->data;
+	dynamic_string_append(data->url, at, length); 
 	return 0;
 }
 
 
 static void read_callback(struct ev_loop *loop, ev_io *watcher, int revents) {
-	char stuff[100000];
+	static char buffer[READSIZE];
 	ssize_t read_length;
+	struct client_socket_data *data = (struct client_socket_data*)watcher->data;
 	
-	read_length = recv(watcher->fd, stuff, 100000, 0);
+	read_length = recv(watcher->fd, buffer, READSIZE, 0);
 	if(read_length == -1) {
 		perror("Could not read");
 		return;
 	}
 	
 	if(read_length == 0) {
-		ev_io_stop(loop, watcher);
-		free(watcher);
-		close(watcher->fd);
-		printf("Connection closed\n");
+		ev_io_stop(data->loop, data->watcher);
+		free(data->watcher);
+		free(data->parser);
+		dynamic_string_free(data->url);
+		close(data->sock);
+		free(data);
+
+		printf("Connection closed by client\n");
 		return;
 	}
 
 	printf("Read %d bytes\n", read_length);
-	printf("%.*s", read_length, stuff);
+	//printf("%.*s", read_length, buffer);
 
-	http_parser_settings parser_settings;
-	parser_settings.on_url = parser_url_callback;
-	http_parser *parser = (http_parser*)malloc(sizeof(http_parser));
-
-	struct http_parser_data *http_parser_data = malloc(sizeof(struct http_parser_data));
-	http_parser_data->loop = loop;
-	http_parser_data->watcher = watcher;
-	parser->data = (void*)http_parser_data;
-	
-	http_parser_init(parser, HTTP_REQUEST);
-	http_parser_execute(parser, &parser_settings, stuff, read_length);
-
+	http_parser_execute(data->parser, &(data->parser_settings), buffer, read_length);
 }
 
 static void accept_callback(struct ev_loop *loop, ev_io *watcher, int revents) {
@@ -79,10 +111,22 @@ static void accept_callback(struct ev_loop *loop, ev_io *watcher, int revents) {
 	char address_str[INET_ADDRSTRLEN];
 	inet_ntop(AF_INET, &(client_addr.sin_addr), address_str, INET_ADDRSTRLEN);
 	printf("Connection accepted from %s\n", address_str);
+	
+	struct client_socket_data *data = malloc(sizeof(struct client_socket_data));
+	data->loop = loop;
+	data->watcher = (ev_io*)malloc(sizeof(ev_io));
+	data->sock = client_sock;
+	data->url = dynamic_string_init();
 
-	ev_io *read_watcher = (ev_io*)malloc(sizeof(ev_io));
-	ev_io_init(read_watcher, read_callback, client_sock, EV_READ);
-	ev_io_start(loop, read_watcher);
+	data->parser_settings.on_url = parser_url_callback;
+	data->parser_settings.on_message_complete = parser_message_complete_callback;
+	data->parser = (http_parser*)malloc(sizeof(http_parser));
+	data->parser->data = (void*)data;
+	http_parser_init(data->parser, HTTP_REQUEST);
+
+	ev_io_init(data->watcher, read_callback, client_sock, EV_READ);
+	data->watcher->data = data;
+	ev_io_start(loop, data->watcher);
 }
 
 void sigint_callback(EV_P_ ev_signal *w, int revents)
@@ -138,8 +182,11 @@ int main()
 	ev_io_start(loop, accept_watcher);
 	ev_run(loop, 0);
 
+	ev_io_stop(loop, accept_watcher);
+	ev_signal_stop(loop, &sigint_watcher);
 	printf("Closing connection\n");
 	free(accept_watcher);
+	ev_loop_destroy(loop);
 	close(sock);
 
 	return 0;
