d898f7d47d78a59d7ffdf3cf26b68b1fb3bef6c0
torque <torque@1>
2013-07-17 01:29:21 -0700

    Scrape support.

--- reki.c
+++ reki.c
@@ -53,6 +53,12 @@ typedef struct {
 } tracker_announce_data;
 
 typedef struct {
+	char **info_hashes;
+	int num;
+	client_socket_data *socket_data;
+} tracker_scrape_data;
+
+typedef struct {
 	int b_length;
 	char bencoded[68]; // maximum possible length
 	char compact[6];
@@ -61,6 +67,13 @@ typedef struct {
 
 static const char *announce_base_url = "/announce?";
 static const char *scrape_base_url = "/scrape?";
+const static char *peer_id_str = "peer_id";
+const static char *info_hash_str = "info_hash";
+const static char *port_str = "port";
+const static char *ip_str = "ip";
+const static char *left_str = "left";
+const static char *compact_str = "compact";
+const static char *event_str = "event";
 redisAsyncContext *redis; // abuse scope for ease-of-use
 
 int intlength(int input) {
@@ -144,32 +157,26 @@ int hex_to_string(char *output, char *input) {
 	int pos,
 	    input_length = strlen(input),
 	    output_length = input_length/2;
-	char *dummy = malloc(sizeof(char)*output_length);
-	check_mem(dummy);
 
 	for (pos = 0; pos < output_length; pos++ ) {
-		long long temp = read_int(input + pos * 2, 2, 16);
-		check(temp != 1, "Failed to parse %.*s as a number.", 2, input + pos * 2);
-		memcpy(dummy + pos, &temp, 1); // this is probably horrible
+		int temp = read_int(input + pos * 2, 2, 16);
+		check(temp != -1, "Failed to parse %.*s as a number.", 2, input + pos * 2);
+		memcpy(output + pos, &temp, 1); // this is probably horrible
 	}
-	output = dummy;
 	return 0;
 
 	error:
-		if(dummy) free(dummy);
 		return -1;
 }
 
-int simple_error(tracker_announce_data *announce_data, char *message) {
-	client_socket_data *data = announce_data->socket_data;
-
+int simple_error(client_socket_data *data, char *message) {
 	int message_length = strlen(message);
 	int failure_reason_length = 20 + message_length + intlength(message_length);
 	int http_response_length = 82 + failure_reason_length + intlength(failure_reason_length);
 
 	char *failure_reason = malloc(sizeof(char)*(failure_reason_length+1));
-	check_mem(failure_reason);
 	char *http_response = malloc(sizeof(char)*(http_response_length+1));
+	check_mem(failure_reason);
 	check_mem(http_response);
 
 	sprintf(failure_reason,"d14:failure reason%u:%se", message_length, message);
@@ -188,7 +195,7 @@ int simple_error(tracker_announce_data *announce_data, char *message) {
 		return -1;
 }
 
-void send_reply(redisAsyncContext *redis, void *r, void *a) {
+void send_announce_reply(redisAsyncContext *redis, void *r, void *a) {
 	redisReply *reply = r;
 	if (reply == NULL) { return; }
 	tracker_announce_data *announce_data = (tracker_announce_data*)a;
@@ -304,7 +311,7 @@ void announce(tracker_announce_data *announce_data) {
 	// if (announce_data->event == 1 ) {
 	// 	// increment completed count
 	// }
-	redisAsyncCommand(redis, send_reply, announce_data, "EXEC");
+	redisAsyncCommand(redis, send_announce_reply, announce_data, "EXEC");
 }
 
 /* This function doesn't even need to exist if we're a pure public tracker */
@@ -326,8 +333,7 @@ void announce(tracker_announce_data *announce_data) {
 // }
 
 int parse_announce_request(client_socket_data *data) {
-	tracker_announce_data *announce_data = malloc(sizeof(tracker_announce_data));
-	memset(announce_data, 0, sizeof(tracker_announce_data));
+	tracker_announce_data *announce_data = calloc(1, sizeof(tracker_announce_data));
 	announce_data->socket_data = data;
 	announce_data->port = -1;
 	announce_data->left = -1;
@@ -340,15 +346,15 @@ int parse_announce_request(client_socket_data *data) {
 			int end_of_token = pos;
 
 			if(beginning_of_token == end_of_token) {
-				printf("No param\n");
+				debug("No param");
 				error = 1;
 			}
 			if(!error & (middle_of_token == -1)) {
-				printf("Missing =\n");
+				debug("Missing =");
 				error = 1;
 			}
 			if(!error & (beginning_of_token == middle_of_token || middle_of_token == end_of_token - 1)) {
-				printf("Missing either field or value\n");
+				debug("Missing either field or value");
 				error = 1;
 			}
 
@@ -358,34 +364,26 @@ int parse_announce_request(client_socket_data *data) {
 				char *value = data->url->str + middle_of_token + 1;
 				int value_size = end_of_token - middle_of_token - 1;
 
-				printf("%.*s = %.*s\n", field_size, field, value_size, value);
+				debug("%.*s = %.*s", field_size, field, value_size, value);
 
-				const static char *peer_id_str = "peer_id";
-				const static char *info_hash_str = "info_hash";
-				const static char *port_str = "port";
-				const static char *ip_str = "ip";
-				const static char *left_str = "left";
-				const static char *compact_str = "compact";
-				const static char *event_str = "event";
-
-				if(strlen(left_str) == field_size && strncmp(left_str, field, strlen(left_str)) == 0) {
-					announce_data->left = read_int(value, value_size, 10);
-
-				} else if(strlen(info_hash_str) == field_size && strncmp(info_hash_str, field, strlen(info_hash_str)) == 0) {
+				if(strlen(info_hash_str) == field_size && strncmp(info_hash_str, field, strlen(info_hash_str)) == 0) {
 					int parsing_succeeded = parse_info_hash(announce_data->info_hash, 40, value, value_size);
 					if (parsing_succeeded == 0) {
 						debug("Info hash: %.*s", 40, announce_data->info_hash);
 					} else {
-						simple_error(announce_data, "Invalid info hash.");
+						simple_error(announce_data->socket_data, "Invalid info hash.");
 						log_info("Invalid hash: %s", value);
 						return 0;
 					}
+
+				} else if(strlen(left_str) == field_size && strncmp(left_str, field, strlen(left_str)) == 0) {
+					announce_data->left = read_int(value, value_size, 10);
 
 				} else if(strlen(port_str) == field_size && strncmp(port_str, field, strlen(port_str)) == 0) {
 					long long temp = read_int(value, value_size, 10);
 					announce_data->port = (int)temp;
 					if(announce_data->port < 0 || announce_data->port > 65335) {
-						simple_error(announce_data, "Invalid port.");
+						simple_error(announce_data->socket_data, "Invalid port.");
 						log_info("Invalid port: %.*s -> %d", value_size, value, announce_data->port);
 					}
 
@@ -411,7 +409,6 @@ int parse_announce_request(client_socket_data *data) {
 						announce_data->event = 2; // 2 for stopped/other things
 						// should make sure that clients don't send events that aren't
 						// started, stopped, or completed.
-						simple_error(announce_data, "Good for you.");
 						return 0;
 					}
 				}
@@ -423,19 +420,19 @@ int parse_announce_request(client_socket_data *data) {
 			error = 0;
 		}
 		else if(data->url->str[pos] == '=') {
-			if(middle_of_token != -1) {
-				printf("Double =\n");
+			if(middle_of_token !=  -1) {
+				debug("Double =");
 				error = 1;
 			}
 			middle_of_token = pos;
 		}
 	}
 	if (announce_data->info_hash[0] == 0) {
-		simple_error(announce_data, "No info_hash specified.");
+		simple_error(announce_data->socket_data, "No info_hash specified.");
 		return 0;
 	}
 	if (announce_data->port == -1) {
-		simple_error(announce_data, "No port specified.");
+		simple_error(announce_data->socket_data, "No port specified.");
 		sentinel("No port.");
 	}
 	if (announce_data->ip == -1) {
@@ -452,6 +449,131 @@ int parse_announce_request(client_socket_data *data) {
 	error:
 		return -1;
 }
+//d5:filesd20:....................d8:completei5e10:downloadedi50e10:incompletei10eeee
+void send_scrape_reply(redisAsyncContext *redis, void *r, void *s) {
+	redisReply *reply = r;
+	if (reply == NULL) { return; }
+	tracker_scrape_data *scrape_data = s;
+	client_socket_data *data = scrape_data->socket_data;
+	dynamic_string *scrape_reply = dynamic_string_init();
+	dynamic_string_append(scrape_reply, "d5:filesd", 8);
+	char hsh[20];
+	char d1[23];
+	char d2[100];
+	for (int i = 0; i < scrape_data->num; i++) {
+		hex_to_string(hsh, scrape_data->info_hashes[i]);
+		memcpy(d1, "20:", 3);
+		memcpy(d1 + 3, hsh, 20);
+		dynamic_string_append(scrape_reply, d1, 23);
+		long long number_of_seeds = reply->element[4*i + 2]->integer;
+		long long number_of_peers = reply->element[4*i + 3]->integer;
+		sprintf(d2, "d8:completei%llde10:downloadedi0e10:incompletei%lldee", number_of_seeds, number_of_peers);
+		dynamic_string_append(scrape_reply, d2, strlen(d2));
+	}
+	dynamic_string_append(scrape_reply, "ee", 2);
+	int reply_size_length = intlength(scrape_reply->size);
+	int http_response_length = 82 + scrape_reply->size + reply_size_length;
+	char *http_response = malloc(http_response_length*sizeof(char));
+	sprintf(http_response, "HTTP/1.0 200 OK\r\nContent-Type: text/plain\r\nConnection: close\r\nContent-Length: %lu\r\n\r\n", scrape_reply->size);
+	memcpy(http_response + 82 + reply_size_length, scrape_reply->str, scrape_reply->size);
+
+	int retval = send(data->sock, http_response, http_response_length, 0);
+	if(retval == -1) {
+		printf("%s\n", strerror(errno));
+	}
+	data->shouldfree = 1;
+}
+
+void scrape(tracker_scrape_data *scrape_data) {
+	time_t now = time(0);
+	redisAsyncCommand(redis, NULL, NULL, "MULTI");
+	for (int i = 0; i < scrape_data->num; i++) {
+		// prune out old entries (peers that haven't announced within DROP_COUNT announce intervals)
+		debug("Scrape: torrent:%s", scrape_data->info_hashes[i]);
+		redisAsyncCommand(redis, NULL, NULL, "ZREMRANGEBYSCORE torrent:%s:seeds 0 %ld", scrape_data->info_hashes[i], now - ANNOUNCE_INTERVAL * DROP_COUNT);
+		redisAsyncCommand(redis, NULL, NULL, "ZREMRANGEBYSCORE torrent:%s:peers 0 %ld", scrape_data->info_hashes[i], now - ANNOUNCE_INTERVAL * DROP_COUNT);
+		// used for complete and incomplete fields in response
+		redisAsyncCommand(redis, NULL, NULL, "ZCARD torrent:%s:seeds", scrape_data->info_hashes[i]);
+		redisAsyncCommand(redis, NULL, NULL, "ZCARD torrent:%s:peers", scrape_data->info_hashes[i]);
+	}
+	redisAsyncCommand(redis, send_scrape_reply, scrape_data, "EXEC");
+}
+
+int parse_scrape_request(client_socket_data *data) {
+	tracker_scrape_data *scrape_data = calloc(1, sizeof(tracker_scrape_data));
+	scrape_data->socket_data = data;
+	scrape_data->info_hashes = malloc(sizeof(char*));
+	scrape_data->info_hashes[0] = calloc(41, sizeof(char));
+	scrape_data->num = 1;
+	int encount = 0;
+
+	int beginning_of_token = strlen(scrape_base_url), middle_of_token = -1, error = 0, pos;
+	for(pos = strlen(scrape_base_url); pos <= data->url->size; pos++) {
+		if(pos == data->url->size || data->url->str[pos] == '&') { // end of string or new param
+			int end_of_token = pos;
+
+			if(beginning_of_token == end_of_token) {
+				debug("No param");
+				error = 1;
+			}
+			if(!error & (middle_of_token == -1)) {
+				debug("Missing =");
+				error = 1;
+			}
+			if(!error & (beginning_of_token == middle_of_token || middle_of_token == end_of_token - 1)) {
+				debug("Missing either field or value");
+				error = 1;
+			}
+
+			if(!error) { // All good
+				char *field = data->url->str + beginning_of_token;
+				int field_size = middle_of_token - beginning_of_token;
+				char *value = data->url->str + middle_of_token + 1;
+				int value_size = end_of_token - middle_of_token - 1;
+
+				debug("%.*s = %.*s", field_size, field, value_size, value);
+
+				if(strlen(info_hash_str) == field_size && strncmp(info_hash_str, field, strlen(info_hash_str)) == 0) {
+					if (encount == scrape_data->num) {
+						scrape_data->num++;
+						scrape_data->info_hashes = realloc(scrape_data->info_hashes, scrape_data->num*sizeof(char *));
+						scrape_data->info_hashes[encount] = calloc(41, sizeof(char));
+					}
+					int parsing_succeeded = parse_info_hash(scrape_data->info_hashes[encount], 40, value, value_size);
+					if (parsing_succeeded == 0) {
+						debug("Info hash: %.*s", 40, scrape_data->info_hashes[encount]);
+						encount++;
+					} else {
+						simple_error(scrape_data->socket_data, "Invalid info hash.");
+						log_info("Invalid hash: %s", value);
+						return 0;
+					}
+				}
+			}
+
+			// Reset
+			beginning_of_token = pos + 1;
+			middle_of_token = -1;
+			error = 0;
+		}
+		else if(data->url->str[pos] == '=') {
+			if(middle_of_token != -1) {
+				debug("Double =");
+				error = 1;
+			}
+			middle_of_token = pos;
+		}
+	}
+	if (scrape_data->info_hashes[0][0] == 0) {
+		simple_error(scrape_data->socket_data, "Not without an info_hash.");
+		sentinel("I don't like your scrape.")
+	}
+	scrape(scrape_data);
+	return 0;
+
+	error:
+		return -1;
+}
 
 static int parser_message_complete_callback(http_parser *parser) {
 	client_socket_data *data = (client_socket_data*)parser->data;
@@ -461,12 +583,13 @@ static int parser_message_complete_callback(http_parser *parser) {
 	inet_ntop(AF_INET, &(data->peer_ip), ip, INET_ADDRSTRLEN);
 	log_info("%s requested %.*s", ip, (int)data->url->size, data->url->str);
 
-	if(data->url->size >= strlen(announce_base_url)) {
+	if (data->url->size >= strlen(announce_base_url)) {
 		if(strncmp(data->url->str, announce_base_url, strlen(announce_base_url)) == 0) {
 			parse_announce_request(data);
+		} else if (strncmp(data->url->str, scrape_base_url, strlen(scrape_base_url)) == 0) {
+			parse_scrape_request(data);
 		}
-	}
-	else {
+	} else {
 		const char *reply = "HTTP/1.0 200 OK\r\nContent-Type: text/text\r\nConnection: close\r\nContent-Length: 2\r\n\r\nHi";
 		send(data->sock, reply, strlen(reply), 0);
 		data->shouldfree = 1;
