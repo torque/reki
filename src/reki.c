7040cd6fa76ca0f79e00426e178cda20b3b0374a
torque <torque@1>
2015-08-24 16:13:32 -0700

    reki: remove commented redis connection code.

--- src/reki.c
+++ src/reki.c
@@ -14,26 +14,9 @@
 #include "dbg.h"
 #include "server.h"
 
-// static void redisConnectCb( const redisAsyncContext *redis, int status ) {
-// 	if ( status != REDIS_OK ) {
-// 		dbg_info( "Error: %s", redis->errstr );
-// 		exit( 1 );
-// 	}
-// 	dbg_info( "Connected to redis." );
-// }
-
-// static void redisDisconnectCb( const redisAsyncContext *redis, int status ) {
-// 	if ( status != REDIS_OK ) {
-// 		dbg_info( "Error: %s", redis->errstr );
-// 		return;
-// 	}
-// 	dbg_info( "Disconnected from redis." );
-// }
-
 static void interruptCb( uv_signal_t *interrupt, int signal ) {
 	puts( "" );
 	log_info( "SIGINT caught. \e[1;31mQuitting\e[m." );
-	// redisAsyncDisconnect( interrupt->data );
 	uv_stop( interrupt->loop );
 }
 
@@ -44,26 +27,15 @@ int main ( int argc, char **argv ) {
 		return 1;
 	}
 
-	// redisAsyncContext *redis = redisAsyncConnect( "localhost", 6379 );
-	// if ( redis->err ) {
-	// 	log_err( "Redis error: %s\n", redis->errstr );
-	// 	free( redis );
-	// 	return 1;
-	// }
-
 	Server *server1 = Server_new( "::", "9001", ServerProtocol_TCP );
 	checkConstructor( server1 );
 	checkFunction( Server_initWithLoop( server1, loop ) );
 	checkFunction( Server_listen( server1 ) );
 
 	uv_signal_t interrupt;
-	// interrupt.data = (void*)redis;
 	uv_signal_init( loop, &interrupt );
 	uv_signal_start( &interrupt, interruptCb, SIGINT );
 
-	// redisLibuvAttach( redis, loop );
-	// redisAsyncSetConnectCallback( redis, redisConnectCb );
-	// redisAsyncSetDisconnectCallback( redis, redisDisconnectCb );
 	uv_run( loop, UV_RUN_DEFAULT );
 	uv_loop_close( loop );
 
